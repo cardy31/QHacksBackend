66703a5d9f0be9115d90260d47d8adef5db6b665
Rob Cardy <rob.cardy.31@gmail.com>
2019-02-01 23:15:05 -0500

    Hide secret key

--- QHacksBackend/settings.py
+++ QHacksBackend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3@@5m-ojymnob8ne4(_o!g@e1jwngd9d06x(+wfno_-31@if(l'
+with open(os.path.join(BASE_DIR, 'QHacksBackend/secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
